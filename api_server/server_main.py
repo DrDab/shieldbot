f408836ea3ddb646cd1f8429e167edd8ca37fd43
_c0da_ <duvictor514@gmail.com>
2019-07-14 12:27:20 -0700

    Fixed formatting

--- api_server/server_main.py
+++ api_server/server_main.py
@@ -69,13 +69,33 @@ def downloadAndParseClassificationResults(s3_result_location, jobid):
    results = f.read()
    return results
 
+# returns json data of jobs.
+def listAllClassificationJobs(maxjobs):
+   toreturndata = "{\"jobs\":["
+   response = comprehend.list_document_classification_jobs(MaxResults=maxjobs)
+   jobList = response["DocumentClassificationJobPropertiesList"]
+   isFirst = False
+   for job in jobList:
+        if isFirst == False:
+		isFirst = True
+	else:
+		toreturndata += ","
+	toreturndata += "["
+	jobid = job["JobId"]
+	jobstatus = job["JobStatus"]
+	toreturndata += "\"" +jobid + "\",\"" + jobstatus + "\"]"
+   toreturndata += "]}"
+   #return toreturndata
+   return json.loads(toreturndata)
+	
+	
 # add the job given the comment and return the job id in JSON form.
 @app.route("/add_job", methods=['GET', 'POST'])
 def add_job():
     inputdata = request.args.get('comment')
     # return the job id
     id = uploadClassificationData(inputdata)
-    return "{\n    \"job-id\":"+id+"\n}"
+    return json.loads("{\n    \"job-id\":\""+id+"\"\n}")
 
 # given the job id, check if the job is done.
 @app.route("/check_job_status", methods=['GET', 'POST'])
@@ -83,25 +103,33 @@ def check_job():
     job_id = request.args.get('jobid')
     # return the job status. (finished, not finished, etc.)
     status = getClassificationJobStatus(job_id)
-    return "{\n    \"status\":"+status+"\n}"
+    return json.loads("{\n    \"status\":\""+status+"\"\n}")
 
 # give the job id, return the classification results of the job.
 @app.route("/get_results", methods=['GET', 'POST'])
 def get_job_results():
     job_id = request.args.get('jobid')
+    if job_id is None:
+	return json.loads("{}")
     # return the job's classification results.
     try:
 	location = getClassificationResultLocation(job_id)
         if(location=="NON_EXISTENT"):
-        	return "{}"
+        	return json.loads("{}")
 
     	data = downloadAndParseClassificationResults(location,job_id)
 
-    	return "{\n    \"results\":"+data+"\n}"
+    	return json.loads(data)
 
     except ClientError as e:
-		return "{}"
+		return json.loads("{}")
     
+@app.route("/list_jobs", methods=['GET', 'POST'])
+def list_jobs():
+	numJobsToList = request.args.get('count')
+	if numJobsToList is None:
+		return listAllClassificationJobs(100)
+	return listAllClassificationJobs(int(numJobsToList))
 
 
 if (__name__ == "__main__"):
@@ -110,8 +138,9 @@ if (__name__ == "__main__"):
     #for bucket in s3.buckets.all():
     #    print(bucket.name)
     #print("Done")
-    # print(uploadClassificationData("furreis r gey"))
+    # print(uploadClassificationData("This is a test message."))
     # print(downloadAndParseClassificationResults(getClassificationResultLocation("98e2ccf561f63051df381747c7bbf7dc"), "98e2ccf561f63051df381747c7bbf7dc"))
     # print(getClassificationJobStatus("98e2ccf561f63051df381747c7bbf7dc"))
     # print(getClassificationJobStatus("1798b4f6315e33fa3725a34c382df4be"))
+    # print(listAllClassificationJobs(100))
     app.run(port = 5000)
